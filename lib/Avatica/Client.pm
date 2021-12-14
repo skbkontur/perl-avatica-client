73f9b8871a39a352efe4a9cc1588249d4ce42399
Aleksey Stavrov <logioniz@ya.ru>
2021-12-14 16:08:45 +0500

    bump new release 0.002

--- lib/Avatica/Client.pm
+++ lib/Avatica/Client.pm
@@ -13,7 +13,7 @@ use Google::ProtocolBuffers::Dynamic 0.30;
 use constant MAX_RETRIES => 1;
 use constant CLASS_REQUEST_PREFIX => 'org.apache.calcite.avatica.proto.Requests$';
 
-our $VERSION = '0.001';
+our $VERSION = '0.002';
 
 sub new {
   my ($class, %params) = @_;
